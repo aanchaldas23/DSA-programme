a0daa66559ecb4c4b094616f12d9e64f86070f25
aanchaldas23 <144616356+aanchaldas23@users.noreply.github.com>
2023-10-07 15:02:01 +0530

    Ques 10: Prime Factorization of a number

---
+++ asg1_ques10.java
@@ -0,0 +1,21 @@
+package Javadsa;
+import java.util.*;
+
+public class asg1_ques10 {
+    public static void main(String[]args){
+        System.out.println("Enter number: ");
+        Scanner z=new Scanner(System.in);
+        int n1=z.nextInt();
+        System.out.println("The prime factors of "+n1+" are: ");
+        for(int i=2;i<=n1;i++)
+        {
+            while(n1%i==0)
+            {
+               n1=n1/i;
+               System.out.println(i);
+            }
+        }
+
+    }
+    
+}
