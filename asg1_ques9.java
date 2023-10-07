167571c10def52fd000f03ace0ec3b0607ed427a
aanchaldas23 <144616356+aanchaldas23@users.noreply.github.com>
2023-10-07 14:57:10 +0530

    1.9 Ques: GCD and LCM of two numbers

---
+++ asg1_ques9.java
@@ -0,0 +1,25 @@
+package Javadsa;
+import java.util.*;
+
+public class asg1_ques9 {
+    public static void main(String[]args){
+        System.out.println("Enter number1: ");
+        Scanner z=new Scanner(System.in);
+        int n1=z.nextInt();
+        System.out.println("Enter number2: ");
+        int n2=z.nextInt();
+        int on1=n1;
+        int on2=n2;
+        while(n1%n2!=0)
+        {
+            int r=n1%n2;
+            n1=n2;
+            n2=r;
+        }
+        int gcd=n2;
+        int lcm=(on1*on2)/gcd;
+        System.out.println("The GCD is "+gcd);
+        System.out.println("The LCM is "+lcm);
+    }
+    
+}
