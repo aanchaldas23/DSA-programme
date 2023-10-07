f8147d400b6c9e2483364ae140b28cb6fc99e9e1
aanchaldas23 <144616356+aanchaldas23@users.noreply.github.com>
2023-10-07 12:50:18 +0530

    Create asg1_ques1.java

---
+++ asg1_ques1.java
@@ -0,0 +1,13 @@
+package Javadsa;
+public class asg1_ques1 {
+
+    public static void main(String[]args){
+        System.out.println("*****");
+        System.out.println("   *");
+        System.out.println("  *");
+        System.out.println(" *");
+        System.out.println("*****");
+
+
+    }
+}
