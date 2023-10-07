bb950ca0c7477a90b53acd1c54d1278145407a99
aanchaldas23 <144616356+aanchaldas23@users.noreply.github.com>
2023-10-07 13:33:02 +0530

    1.2 Ques: Grading System

---
+++ asg1_ques2.java
@@ -0,0 +1,36 @@
+package Javadsa;
+import java.util.*;
+
+public class asg1_ques2 {
+    public static void main(String[] args){
+
+        Scanner z=new Scanner(System.in);
+        System.out.println("Enter your grade:");
+        int q=z.nextInt();
+        if(q>90)
+        {
+            System.out.println("Excellent");
+        }
+        else if(q>80)
+        {
+            System.out.println("good");
+        }
+        else if(q>70)
+        {
+            System.out.println("fair");
+        }
+        else if(60<q)
+        {
+            System.out.println("meets expectations");
+        }
+        else
+        {
+            System.out.println("below par");
+        }
+
+    
+    }
+
+}
+
+
